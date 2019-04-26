ec1504016f0e393aeeceae53521b2dc59a17fb53
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-04-26 12:44:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,21 +11,5 @@ client.on('message', message => {
   	}
 });
 
-bot.on("guildMemberAdd", member => {
-
-    const channel = member.guild.channels.find("name", "report");
-    if (!channel) console.log("Kan het kanaal niet vinden.");
-
-    var joinEmbed = new discord.RichEmbed()
-        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
-        .setDescription(`Hoi ${member.user.username}, **Welkom op de server**. Hier nog meer uitleg.`)
-        .setColor("#00FF00")
-        .setTimestamp()
-        .setFooter("Gebruiker gejoined.");
-
-    channel.send(joinEmbed);
-
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
