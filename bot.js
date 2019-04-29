0b74127df040113297fdac65b1fd97987b905616
Tryharq <48394988+Tryharq@users.noreply.github.com>
2019-04-29 19:49:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,13 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 
+});
+//Speelt
+bot.on("ready", async () => { 
+
+    console.log(`${bot.user.username} is online on ${bot.guilds.size} server(s) !`);
+
+    bot.user.setActivity(`Helpen op TownHCF!`, { type: "PLAYING"});
 
 
 });
